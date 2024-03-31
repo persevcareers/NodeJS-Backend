25b15fbd46abd4048aa50e82ab273e1544f5dc25
persevcareers <148257733+persevcareers@users.noreply.github.com>
2024-03-31 18:40:46 +0530

    Update db.js

--- db.js
+++ db.js
@@ -3,19 +3,16 @@ const mongoose = require("mongoose");
 module.exports = async () => {
     try {
         const connectionParams = {
-            // user: process.env.MONGO_USERNAME,
-            // pass: process.env.MONGO_PASSWORD,
             useNewUrlParser: true,
-            // useCreateIndex: true,
             useUnifiedTopology: true,
         };
         const useDBAuth = process.env.USE_DB_AUTH || false;
         if(useDBAuth){
-            connectionParams.user = process.env.MONGO_USERNAME;
-            connectionParams.pass = process.env.MONGO_PASSWORD;
+            connectionParams.user = process.env.DB_USERNAME;
+            connectionParams.pass = process.env.DB_PASSWORD;
         }
         await mongoose.connect(
-           process.env.MONGO_CONN_STR,
+           process.env.DB_CONN_STR,
            connectionParams
         );
         console.log("Connected to database.");
